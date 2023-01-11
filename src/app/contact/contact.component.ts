d51bc4ab6cba62d5b0a2d56cf46d15f97c7a1bf6
Michael Dunn <michaeldunn@mac.attlocal.net>
2023-01-11 15:41:46 -0600

    Fixes email

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -6,9 +6,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactCompnent {
+  data = <any>{};
 
-  sendEmail() {
-    console.log('working?')
+  sendEmail(data) {
+    const mapForm = document.createElement("form");
+    mapForm.method = "POST";
+    mapForm.action = "https://formsubmit.co/medunn626@yahoo.com";
+
+    // Name:
+    const nameInput = document.createElement("input");
+    nameInput.type = "text";
+    nameInput.name = "name";
+    nameInput.value = data.name;
+    mapForm.appendChild(nameInput);
     
+    // Email:
+    const emailInput = document.createElement("input");
+    emailInput.type = "email";
+    emailInput.name = "email";
+    emailInput.value = data.email;
+    mapForm.appendChild(emailInput);
+
+    // Text:
+    const messageInput = document.createElement("textarea");
+    messageInput.name = "message";
+    messageInput.value = data.message;
+    mapForm.appendChild(messageInput);
+
+    // Subject:
+    const subjectInput = document.createElement("input");
+    subjectInput.type = "hidden";
+    subjectInput.name = "_subject";
+    subjectInput.value = "New Client Email!"
+    mapForm.appendChild(subjectInput);
+
+    // Next:
+    const nextInput = document.createElement("input");
+    nextInput.type = "hidden";
+    nextInput.name = "_next";
+    nextInput.value = "https://medunn626.github.io/top-king/contact"
+    mapForm.appendChild(nextInput);
+
+    document.body.appendChild(mapForm);
+
+    mapForm.submit();  
   }
 }
