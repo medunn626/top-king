c2872170d73eefc97077073d3e5c3d916e664e47
Michael Dunn <michaeldunn@mac.attlocal.net>
2023-01-13 15:58:45 -0600

    Populate content component

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+interface Video {
+  plansApplies: number[];
+  title?: string;
+  source?: string;
+}
 
 @Component({
   selector: 'app-content',
@@ -6,10 +13,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./content.component.css']
 })
 export class ContentComponent implements OnInit {
-
-  constructor() { }
+  usersCurrentPlan = 0;
+  videos: Video[];
+  userAccessVideos: Video[];
+  latestUserHiddenVideo: Video;
+  
+  constructor(
+    public router: Router,
+  ) { }
 
   ngOnInit() {
+    this.loadVideoData();
+    this.usersCurrentPlan = +(localStorage.getItem('plan') ?? 0);
+    this.userAccessVideos = this.videos.filter(vid => vid.plansApplies.includes(this.usersCurrentPlan));
+    const userHiddenVideos = this.videos.filter(vid => !vid.plansApplies.includes(this.usersCurrentPlan));
+    this.latestUserHiddenVideo = userHiddenVideos[userHiddenVideos.length - 1];
+  }
+
+  // Mock right now but would grab from a service 
+  loadVideoData() {
+    this.videos = [
+      {
+        title: 'Compeition',
+        source: '../../assets/competition.mp4',
+        plansApplies: [1, 2, 3]
+      },
+      {
+        title: 'Grocery Shopping',
+        source: '../../assets/grocery.mp4',
+        plansApplies: [2, 3]
+      },
+      {
+        title: 'Cheat Meal',
+        source: '../../assets/grocery.mp4',
+        plansApplies: [3]
+      },
+      {
+        title: 'Powerball',
+        source: '../../assets/powerball.mp4',
+        plansApplies: [3]
+      }
+    ]
   }
 
 }
